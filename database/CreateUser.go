494e7c2b761ff59fcf2eee4cdc0ac680df5e59d9
Ali Hassan Zahid <162284502+ALIHASSAN0090@users.noreply.github.com>
2024-11-22 09:44:20 +0500

    [feat] added dockerfile + yml file -> dockerized

--- database/CreateUser.go
+++ database/CreateUser.go
@@ -9,8 +9,6 @@ import (
 
 func CreateUser(c *gin.Context, User models.User) (models.User, error) {
 
-	log.Printf("Received user credentials: Username: %s, Email: %s, FirstName: %s, LastName: %s", User.Username, User.Email, User.FirstName, User.LastName)
-
 	query := `INSERT INTO users (username, email, password_hash, first_name, last_name) 
 VALUES ($1, $2, $3, $4, $5) RETURNING id, created_at;`
 	err := DbConn.QueryRow(query, User.Username, User.Email, User.PasswordHash, User.FirstName, User.LastName).Scan(&User.ID, &User.CreatedAt)
